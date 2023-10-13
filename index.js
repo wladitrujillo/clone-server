d59642e49566382eb1013eb1afb2d8ee3b193d25
Wladimir Trujillo <wladimir.trujillo@outlook.com>
2023-10-12 22:22:28 -0500

    fix: angular app

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+var path = require('path');
 var express = require('express');
 var app = express();
 
 //setting middleware
 app.use(express.static('public')); //Serves resources from public folder
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
-var server = app.listen(8080, ()=> console.log('Server is running on port 8080'));
+var server = app.listen(8080, () => console.log('Server is running on port 8080'));
